4eae5b5375e3dc4ec84e09d7347e14e793750133
xixizhang96 <xixizhang96@gmail.com>
2017-06-22 16:32:23 +0800

    生成图片路径并引入到页面上

--- src/components/Main_201706221532.js
+++ src/components/Main_201706221532.js
@@ -3,29 +3,27 @@ require('styles/App.scss');
 
 import React from 'react';
 
-// let yeomanImage = require('../images/yeoman.png');
-
 //获取图片相关的数据
-let imageDatas = require('../data/imageDatas.json');
+let imageDatas = require('json!../data/imageDatas.json');
 
 //利用自执行函数，将图片名信息转成图片URL路径信息
-imageDatas = (function genImageURL(imageDatasArr) {
-  for (var i = 0, j = imageDatasArr.length; i < j; i++) {
-    var singleImageData = imageDatasArr[i];
-    singleImageData.imageURL = require('../images/' + singleImageData.fileName);
-
-    imageDatasArr[i] = singleImageData;
-  }
-  return imageDatasArr;
-})(imageDatas);
 // imageDatas = (function genImageURL(imageDatasArr) {
 //   for (var i = 0, j = imageDatasArr.length; i < j; i++) {
 //     var singleImageData = imageDatasArr[i];
 //     singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+
 //     imageDatasArr[i] = singleImageData;
 //   }
 //   return imageDatasArr;
 // })(imageDatas);
+imageDatas = (function genImageURL(imageDatasArr) {
+  for (var i = 0, j = imageDatasArr.length; i < j; i++) {
+    var singleImageData = imageDatasArr[i];
+    singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+    imageDatasArr[i] = singleImageData;
+  }
+  return imageDatasArr;
+})(imageDatas);
 
 class ImgFigure extends React.Component {
   render() {
@@ -58,19 +56,19 @@ class AppComponent extends React.Component {
     //   console.log(value);
     // });
 
-    Array.prototype.forEach.call(imageDatas, (value, index)  =>  {          
-      imgFigures.push(<ImgFigure data = {value} key = {'imgFigures' + index} />);    
+    Array.prototype.forEach.call(imageDatas, (value)  =>  {          
+      imgFigures.push(<ImgFigure data = {value} />);    
     });
 
     return (
       <section className="stage">
         <section className="img-sec">
           {imgFigures}
-        </section> 
+        </section>
         <nav className="controller-nav">
           {controllerUnits}
-        </nav>    
-      </section >
+        </nav> 
+      </section>
     );
   }
 }
