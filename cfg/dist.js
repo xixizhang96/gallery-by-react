bfb960bcce6f1ecd140577226f1e97e534c6eb96
xixizhang96 <xixizhang96@gmail.com>
2017-06-29 14:02:04 +0800

    update 路径问题，需要手动修改app.js的值

--- cfg/dist.js
+++ cfg/dist.js
@@ -1,42 +1,41 @@
-'use strict';
+  'use strict';
 
-let path = require('path');
-let webpack = require('webpack');
+  let path = require('path');
+  let webpack = require('webpack');
 
-let baseConfig = require('./base');
-let defaultSettings = require('./defaults');
+  let baseConfig = require('./base');
+  let defaultSettings = require('./defaults');
 
-// Add needed plugins here
-let BowerWebpackPlugin = require('bower-webpack-plugin');
+  // Add needed plugins here
+  let BowerWebpackPlugin = require('bower-webpack-plugin');
 
-let config = Object.assign({}, baseConfig, {
-  entry: path.join(__dirname, '../src/index'),
-  cache: false,
-  devtool: 'sourcemap',
-  plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"'
-    }),
-    new BowerWebpackPlugin({
-      searchResolveModulesDirectories: false
-    }),
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.AggressiveMergingPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
-  module: defaultSettings.getDefaultModules()
-});
+  let config = Object.assign({}, baseConfig, {
+    entry: path.join(__dirname, '../src/index'),
+    cache: false,
+    devtool: 'sourcemap',
+    plugins: [
+      new webpack.optimize.DedupePlugin(),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': '"production"'
+      }),
+      new BowerWebpackPlugin({
+        searchResolveModulesDirectories: false
+      }),
+      new webpack.optimize.UglifyJsPlugin(),
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.optimize.AggressiveMergingPlugin(),
+      new webpack.NoErrorsPlugin()
+    ],
+    module: defaultSettings.getDefaultModules()
+  });
 
-// Add needed loaders to the defaults here
-config.module.loaders.push({
-  test: /\.(js|jsx)$/,
-  loader: 'babel',
-  include: [].concat(
-    config.additionalPaths,
-    [ path.join(__dirname, '/../src') ]
-  )
-});
+  // Add needed loaders to the defaults here
+  config.module.loaders.push({
+    test: /\.(js|jsx)$/,
+    loader: 'babel',
+    include: [].concat(
+      config.additionalPaths, [path.join(__dirname, '/../src')]
+    )
+  });
 
-module.exports = config;
+  module.exports = config;
